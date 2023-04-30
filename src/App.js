4677dab8c7c384712af667531d77079efeec2025
Minlyy <minsong223@gmail.com>
2023-04-30 17:37:45 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,115 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function CatFact() {
+  const [fact, setFact] = useState('');
+
+  const fetchFact = async () => {
+    try {
+      const response = await fetch('https://cat-fact.herokuapp.com/facts/random');
+      const data = await response.json();
+      setFact(data.text)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFact();
+  }, []);
+
+  return (
+    <div className='mt-6 mb-6 w-3/6'>
+      <h2>Random Cat Fact:</h2>
+      <p className='mx-auto'>{fact}</p>
+      <button onClick={fetchFact} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">New Fact</button>
+    </div>
+  );
+}
+
+function CatGrid() {
+  const [catBreeds, setCatBreeds] = useState([]);
+  const [catIndex, setIndex] = useState(0);
+  const [favorites, setFavorites] = useState([]);
+
+  const fetchCatImages = async () => {
+    try {
+      const response = await fetch('https://api.thecatapi.com/v1/breeds');
+      const data = await response.json();
+      const actualData = [];
+      for(let i = 0; i < 9; i++) {
+        const response2 = await fetch('https://api.thecatapi.com/v1/images/search?breed_id='+data[catIndex + i].id);
+        const data2 = await response2.json();
+        console.log(data2)
+        if(data2.length != 0)
+        {
+          data[catIndex + i]['url'] = data2[0].url;
+          actualData.push(data[catIndex + i]);
+        }
+        else
+        {
+          data[catIndex + i]['url'] = "";
+          actualData.push(data[catIndex + i]);
+        }
+      }
+      setCatBreeds(actualData);
+    } catch (error) {
+      console.log(error);
+    }
+    setIndex(catIndex + 9);
+  };
+
+
+  useEffect(() => {
+    fetchCatImages();
+  }, []);
+  
+  function AddFavorite(url, breed) {
+    let cat = {};
+    cat['url'] = url;
+    cat['breed'] = breed;
+    setFavorites([...favorites, cat])
+  }
+
+  return (
+    <div>
+    <div className="grid grid-cols-3 grid-rows-3 gap-4 w-1/2 mx-auto">
+      {catBreeds.map(catBreed => (
+        <div>
+          <img src={catBreed.url} alt={catBreed.name} className='w-full h-80 object-cover' onClick={() => AddFavorite(catBreed.url, catBreed.name)}/>
+          <p>{catBreed.name}</p>
+        </div>
+      ))}
+      <button onClick={fetchCatImages} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded col-end-4 mb-6">New Breeds</button>
+    </div>
+    <h1 className='mb-5 mt-5'>Favorites</h1>
+      <Favorite favorites={favorites} />
+    </div>
+
+  );
+};
+
+function Favorite({favorites}) {
+  return (
+    <div className='grid grid-cols-4 grid-rows-4 gap-4 ml-4 mr-4'>
+      {favorites.map(cat => (
+        <img src={cat.url} className='w-full h-80 object-cover'></img>
+      ))}
+    </div>
+  );
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='mt-4'>Cats</h1>
+        <CatFact></CatFact>
+        <h1>Click to add to favorite</h1>
+        <CatGrid></CatGrid>
       </header>
     </div>
   );
